13f0fb9237b2051761afd56c2a4f5d1960f2372c
Devesh <85085505+depgh431@users.noreply.github.com>
2024-07-31 23:26:52 +0530

    Update builder.py

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -8,8 +8,6 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
 
     kwargs = {}
 
-    if device != "cuda":
-        kwargs['device_map'] = {"": device}
 
     if load_8bit:
         kwargs['load_in_8bit'] = True
@@ -30,8 +28,9 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
                 tokenizer = AutoTokenizer.from_pretrained(model_path)
                 model = LlavaMistralForCausalLM.from_pretrained(
                     model_path,
-                    low_cpu_mem_usage=False,
+                    low_cpu_mem_usage=True,
                     use_flash_attention_2=False,
+                    device_map="cuda",
                     **kwargs
                 )
     else:
