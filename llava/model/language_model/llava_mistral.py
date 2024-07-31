3eb038bc5f6f379e96c1f0ab6f0795e842f62410
Devesh <85085505+depgh431@users.noreply.github.com>
2024-07-31 23:27:38 +0530

    Update llava_mistral.py

--- llava/model/language_model/llava_mistral.py
+++ llava/model/language_model/llava_mistral.py
@@ -40,13 +40,14 @@ class LlavaMistralForCausalLM(MistralForCausalLM, LlavaMetaForCausalLM):
 
     def forward(
         self,
+	use_cache: None = None,
+        cache_position: None =None,
         input_ids: torch.LongTensor = None,
         attention_mask: Optional[torch.Tensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
         past_key_values: Optional[List[torch.FloatTensor]] = None,
         inputs_embeds: Optional[torch.FloatTensor] = None,
         labels: Optional[torch.LongTensor] = None,
-        use_cache: Optional[bool] = None,
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         images: Optional[torch.FloatTensor] = None,
@@ -79,7 +80,6 @@ class LlavaMistralForCausalLM(MistralForCausalLM, LlavaMetaForCausalLM):
             past_key_values=past_key_values,
             inputs_embeds=inputs_embeds,
             labels=labels,
-            use_cache=use_cache,
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict
